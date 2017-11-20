9278e4057a37364bfb92585b2f0d12bcfc6459f5
VladimirV92 <vladimirvalchev.92@gmail.com>
2017-11-20 16:45:55 +0200

    countCommand class

---
+++ Commands/CountCommand.cs
@@ -0,0 +1,17 @@
+﻿using ListProcessing.Interfaces;
+
+namespace ListProcessing.Commands
+{
+    class CountCommand : Command
+    {
+        public CountCommand(string element, IListRepossitory listRepository, int index)
+            : base(element, listRepository, index)
+        {
+
+        }
+        public override void Execute()
+        {
+            this.ListRepossitory.Count(this.Element);
+        }
+    }
+}
