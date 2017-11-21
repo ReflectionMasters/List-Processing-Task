00c5a9cb00b64360e45aee5a2d19c6aba3121197
Zlatyo Uzunov <zluzunov@gmail.com>
2017-11-21 22:25:35 +0200

    added parameters exception;

--- Exceptions.cs
+++ Exceptions.cs
@@ -4,5 +4,6 @@
     {
         public const string InvalidCommandException = "Error: invalid command";
         public const string InvalidIndexException = "Error: invalid index {0}";
+        public const string InvalidParametersException = "Error: invalid command parameters";
     }
 }
